ddd9eb38ebfe285f629c57931f653f9996b20ee0
lmurphy1 <lmurphy1@stevens.edu>
2020-02-02 14:14:09 -0500

    Revision 1

--- python_refresher.py
+++ python_refresher.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Laura Murphy
 FE 595 Python Refresher
@@ -7,10 +6,15 @@ FE 595 Python Refresher
 import numpy as np
 import matplotlib.pyplot as plt
 
-x1 = np.linspace(0, 2*np.pi)
-x2 = np.linspace(0,2*np.pi)
-sin = plt.plot(x1, np.sin(x1), color = "red", label="sin")
-cos = plt.plot(x2, np.cos(x2), color = "yellow", label="cos")
-plt.xlabel('Angle (radians)')
-plt.legend()
-plt.show()
+
+def plot():
+    x = np.linspace(0, 2 * np.pi, num=500)
+    plt.plot(x, np.sin(x), color="red", label="sin")
+    plt.plot(x, np.cos(x), color="blue", label="cos")
+    plt.xlabel("Angle (radians)")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    plot()
